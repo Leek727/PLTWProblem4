fa4d9b695b865a337e7b9e1ec8658c6755fbc0af
Jason Wang <91506829+Leek727@users.noreply.github.com>
2023-05-22 18:23:50 -0400

    Added more demo functions

--- grapher.java
+++ grapher.java
@@ -20,15 +20,16 @@ public class grapher extends JPanel {
           // not a virus i promise
           System.out.println("Calculating points...");
           System.out.println("Running JS...");
-          String calculateScript = "var points = []; var x = 0; var y = 0;                             " +
-                    "for (var i = 0; i < " + rangeX * 100 + "; i++) {                     " +
-                    "points[i] = [];                                                " +
-                    "for (var j = 0; j < " + rangeY * 100 + "; j++) {                 " +
-                    "x = i / 100 - " + rangeX / 2 + "; y = j / 100 -" + rangeY / 2 + "; " +
-                    "points[i][j] = eval(\"" + func + "\")                      " +
-                    "}                                                              " +
-                    "}                                                                  " +
-                    "points.toString();                                                 ";
+          String calculateScript = 
+               "var points = []; var x = 0; var y = 0;                                         " +
+               "for (var i = 0; i < " + rangeX * 100 + "; i++) {                               " +
+               "    points[i] = [];                                                            " +
+               "    for (var j = 0; j < " + rangeY * 100 + "; j++) {                           " +
+               "         x = i / 100 - " + rangeX / 2 + "; y = j / 100 -" + rangeY / 2 + ";    " +
+               "         points[i][j] = eval(\"" + func + "\")                                 " +
+               "    }                                                                          " +
+               "}                                                                              " +
+               "points.toString();                                                             ";
 
           try {
                // create a evaluable string for js engine, and return the output
@@ -39,7 +40,6 @@ public class grapher extends JPanel {
                return "";
           }
      }
-
 
      // graphing / rendering function
      public void draw(Graphics g) {
@@ -75,6 +75,13 @@ public class grapher extends JPanel {
                }
           }
 
+          if (max > 50){
+               max = 50;
+          }
+          if (min < -50){
+               min = -50;
+          }
+
           System.out.println("Rendering Top View...");
           // render 2d point array
           float hueScale = 0.0f;
@@ -96,12 +103,13 @@ public class grapher extends JPanel {
           System.out.println("Rendering contour lines...");
           // iterate from mix to max of step size contour_res and render points that are
           // close to the current step
-          System.out.println("Max value: " + Math.round(max * 100) / 100 + "\nMin value: " + Math.round(min * 100) / 100);
+          System.out.println(
+                    "Max value: " + Math.round(max * 100) / 100 + "\nMin value: " + Math.round(min * 100) / 100);
           contour_res = (max - min) / 5.0;
 
           g.setColor(Color.WHITE);
           for (double i = min; i < max; i += contour_res) {
-               for (int x = 0; x < width/2; x++) {
+               for (int x = 0; x < width / 2; x++) {
                     for (int y = 0; y < height; y++) {
                          if (points[x][y] < i + contour_res / 100 && points[x][y] > i - contour_res / 100) {
                               drawPoint(x, height - y, g);
@@ -111,7 +119,7 @@ public class grapher extends JPanel {
           }
 
           System.out.println("Rendering Side View...");
-          
+
           // draw side view
           for (int x = 0; x < width / 2; x++) {
                for (int y = 0; y < height; y++) {
@@ -135,19 +143,19 @@ public class grapher extends JPanel {
           g.drawLine(0, height / 2, width, height / 2);
 
           // draw text
-          g.setFont(new Font("Helvetica", Font.BOLD, 17)); 
+          g.setFont(new Font("Helvetica", Font.BOLD, 17));
           g.setColor(Color.BLACK);
-          g.drawString("Height Map Top View", width/4 - 100, 80);
-          g.drawString("Height Map Side View", width - width/4 - 100, 80);
-     
+          g.drawString("Height Map Top View", width / 4 - 100, 80);
+          g.drawString("Height Map Side View", width - width / 4 - 100, 80);
+
           // axes test
           g.setFont(new Font("Helvetica", Font.BOLD, 12));
-          g.drawString("x", 20, height/2 - 10);
-          g.drawString("y", width/4 + 10, 50);
+          g.drawString("x", 20, height / 2 - 10);
+          g.drawString("y", width / 4 + 10, 50);
 
-          g.drawString("x", 20 + width/2, height/2 - 10);
-          g.drawString("y", width/4 + 10 + width/2, 50);
-     
+          g.drawString("x", 20 + width / 2, height / 2 - 10);
+          g.drawString("y", width / 4 + 10 + width / 2, 50);
+
           System.out.println("Done!");
      }
 
@@ -160,9 +168,11 @@ public class grapher extends JPanel {
 
           // get user input for function
           System.out.println("     ==========================================================");
-          System.out.println("Welcome to the best 3d graphing program ever!\nThe left side is a top view of the graph, and the right side is a side view.\nThe color gradient represents the height of the function at that point.");
+          System.out.println(
+                    "Welcome to the best 3d graphing program ever!\nThe left side is a top view of the graph, and the right side is a side view.\nThe color gradient represents the height of the function at that point.");
           System.out.println("     ==========================================================");
-          System.out.println("The function is inputted as a JavaScript expression of x and y.\nFor example, x*x+y*y would be a paraboloid.\nBe sure to use Math.cos(x) instead of cos(x) and Math.sin(x) instead of sin(x).");
+          System.out.println(
+                    "The function is inputted as a JavaScript expression of x and y.\nFor example, x*x+y*y would be a paraboloid.\nBe sure to use Math.cos(x) instead of cos(x) and Math.sin(x) instead of sin(x).");
           System.out.println("     ==========================================================");
           System.out.print("Enter your function (in JavaScript syntax) or press enter for a cool random function: ");
 
@@ -171,11 +181,13 @@ public class grapher extends JPanel {
           inp.close();
 
           if (graph.func.equals("")) {
-               String coolFunctions[] = {"Math.cos(x)*Math.sin(y)", "x*x-y*y", "x*x+y*y", "Math.sin(2 * (x*x + y*y))"};
-               String coolFuncNames[] = {"waves", "saddle", "paraboloid", "ripple"};
+               String coolFunctions[] = { "Math.cos(x)*Math.sin(y)", "x*x-y*y", "x*x+y*y",
+                         "Math.sin(2 * (x*x + y*y))", "x*y*y*y-y*x*x*x", "-1/(x*x+y*y)"};
+               String coolFuncNames[] = { "waves", "saddle", "paraboloid", "ripple", "spin???","Gabriel's Horn"};
                int coolIndex = new Random().nextInt(coolFunctions.length);
                graph.func = coolFunctions[coolIndex];
-               System.out.println("\n\n\nChosen function: " + graph.func + "\n" + "Function name: " + coolFuncNames[coolIndex] + "\n");
+               System.out.println("\n\n\nChosen function: " + graph.func + "\n" + "Function name: "
+                         + coolFuncNames[coolIndex] + "\n");
           }
 
           // awt setup
